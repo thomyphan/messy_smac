e99a641d617e99eabf593cee52d79bb2dd8f54ff
thomy.phan <Quisut0m>
2023-01-24 14:28:02 +0100

    Update setup

--- src/src/run_gen_init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python3 generate_init_states.py 3s5z 1000
-python3 generate_init_states.py 1c3s5z 1000
-python3 generate_init_states.py 5m_vs_6m 1000
-python3 generate_init_states.py bane_vs_bane 1000
